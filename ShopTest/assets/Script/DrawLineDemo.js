2ff675338b20c89fb7eff10e468684cef32a8f6e
火冰凝 <leesongfan@qq.com>
2018-11-20 10:13:10 +0800

    效果图 画线

---
+++ ShopTest/assets/Script/DrawLineDemo.js
@@ -0,0 +1,28 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {},
+
+    // use this for initialization
+    onLoad: function () {
+
+        this._drawLine();
+    },
+
+    _drawLine: function () {
+
+
+        let gNode = cc.find("demo/graphic",this.node);
+        this.graphics = gNode.getComponent(cc.Graphics);
+        this.graphics.strokeColor = cc.hexToColor('#ff0000');//绘制线的颜色
+        this.graphics.lineCap = cc.Graphics.LineCap.ROUND;//线段末端属性,带圆角
+        this.graphics.lineJoin = cc.Graphics.LineJoin.ROUND;//线段拐角 (连接处)属性
+        this.graphics.lineWidth = 10;//线的宽度
+        this.graphics.moveTo(-20, 0);//画线起点坐标
+        this.graphics.lineTo(0, 100);//画线终点坐标
+        this.graphics.stroke();//必须调用用于画线
+
+    },
+
+
+});
